3937d40cd7efcf79dac1a432a5278dc31610d5c7
Tommy <tom@mechanicalape.com>
2013-06-30 18:43:32 -0400

    use django/jinja for liquid highlighting

---
+++ _plugins/vimeo.rb
@@ -0,0 +1,17 @@
+#from https://github.com/optikfluffel/octopress-responsive-video-embed/blob/master/vimeo.rb
+
+module Jekyll
+  class Vimeo < Liquid::Tag
+
+    def initialize(name, id, tokens)
+      super
+      @id = id
+    end
+
+    def render(context)
+      %(<div class="embed-video-container"><iframe src="http://player.vimeo.com/video/#{@id}"></iframe></div>)
+    end
+  end
+end
+
+Liquid::Template.register_tag('vimeo', Jekyll::Vimeo)
